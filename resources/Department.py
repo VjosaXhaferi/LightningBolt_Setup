8da5f4aa8104dc001ab6da92649382172589a8cd
VjosaXhaferi <vjosaxhaferi@Vjosas-MacBook-Pro.local>
2023-02-27 17:00:16 +0100

    fixes template name

--- resources/Department.py
+++ resources/Department.py
@@ -1,5 +1,5 @@
 class Department:
     url = "https://s2.usw2.qa.lightning-bolt.com/Setup.aspx?page=departments"
-    departmentName = "Ritech Department: "
-    templateName = "Ritech Template: "
-    templateDescription = "Ritech Description: "
+    departmentName = "Ritech Department "
+    templateName = "Ritech Template "
+    templateDescription = "Ritech Description "
