f48874656d90d6388da3e3c50a097dff6959928b
VjosaXhaferi <vjosaxhaferi@Vjosas-MacBook-Pro.local>
2023-02-27 11:38:22 +0100

    Adding random data

--- resources/Department.py
+++ resources/Department.py
@@ -1,5 +1,5 @@
 class Department:
     url = "https://s2.usw2.qa.lightning-bolt.com/Setup.aspx?page=departments"
-    departmentName = "Test Case"
-    templateName = "Test Case"
-    templateDescription = "Test Case"
+    departmentName = "Ritech Department: "
+    templateName = "Ritech Template: "
+    templateDescription = "Ritech Description: "
--- tests/test_AddDepartment.py
+++ tests/test_AddDepartment.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from pageObjects.LoginPage import LogIn
 from resources.Department import Department
-
+import random
 
 class TestLogingfeature:
     def test_LogInFeature(self):
@@ -11,11 +11,13 @@ class TestLogingfeature:
         self.driver.find_element(By.LINK_TEXT, "Departments").click()
         assert self.driver.current_url == Department.url
 
+        randInt = random.randint(0, 100)
+
         self.driver.switch_to.frame(0)
         self.driver.find_element(By.XPATH, "//input[@value='+ New Department']").click()
-        self.driver.find_element(By.ID, "department_DeptName").send_keys(Department.departmentName)
-        self.driver.find_element(By.ID, "NewTemplateName").send_keys(Department.templateName)
-        self.driver.find_element(By.ID, "NewTemplateDescription").send_keys(Department.templateDescription)
+        self.driver.find_element(By.ID, "department_DeptName").send_keys(Department.departmentName + str(randInt))
+        self.driver.find_element(By.ID, "NewTemplateName").send_keys(Department.templateName + str(randInt))
+        self.driver.find_element(By.ID, "NewTemplateDescription").send_keys(Department.templateDescription + str(randInt))
         self.driver.find_element(By.ID, "add_button").click()
         self.driver.find_element(By.XPATH, "//input[@class='orange-button right save_changes']").click()
 
