7dec67cfe009a1053a9d198ff2b9cb6b1dbb2131
VjosaXhaferi <vjosaxhaferi@Vjosas-MacBook-Pro.local>
2023-02-28 13:50:05 +0100

    Checks assertion

--- tests/test_AddDepartment.py
+++ tests/test_AddDepartment.py
@@ -15,12 +15,22 @@ class TestLogingfeature:
 
         self.driver.switch_to.frame(0)
         self.driver.find_element(By.XPATH, "//input[@value='+ New Department']").click()
-        self.driver.find_element(By.ID, "department_DeptName").send_keys(Department.departmentName + str(randInt))
+        new_department = Department.departmentName + str(randInt)
+        self.driver.find_element(By.ID, "department_DeptName").send_keys(new_department)
         self.driver.find_element(By.ID, "NewTemplateName").send_keys(Department.templateName + str(randInt))
         self.driver.find_element(By.ID, "NewTemplateDescription").send_keys(Department.templateDescription + str(randInt))
         self.driver.find_element(By.ID, "add_button").click()
         self.driver.find_element(By.XPATH, "//input[@class='orange-button right save_changes']").click()
 
-        #table = self.driver.find_element(By.XPATH, "//table[@id='departments']")
+        table = self.driver.find_element(By.XPATH, "//table[@id='departments']")
+
+        row = None
+        for tr in table.find_elements(By.TAG_NAME, 'tr'):
+            if new_department in tr.text:
+                row = tr
+                break
+
+        # Assert that the item was added to the table
+        assert row is not None, f"Item '{new_department}' not found in table"
 
 
