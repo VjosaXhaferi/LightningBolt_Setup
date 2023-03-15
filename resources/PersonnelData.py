43b943c2c81e94fb5a89fa494beb1a9b1029c949
VjosaXhaferi <vjosaxhaferi@Vjosas-MacBook-Pro.local>
2023-03-15 15:09:43 +0100

    Adds report file

--- resources/PersonnelData.py
+++ resources/PersonnelData.py
@@ -6,8 +6,9 @@ class Personnel:
     url = "https://s2.usw2.qa.lightning-bolt.com/Setup.aspx?page=personnel"
     LastName = "Adams"
     FirstName = "Ken"
-    DisplayNameGenerator = random.choices(string.ascii_lowercase, k=5)
-    DisplayName = ''.join(DisplayNameGenerator)
+    # DisplayNameGenerator = random.choices(string.ascii_lowercase, k=5)
+    # DisplayName = ''.join(DisplayNameGenerator)
+    DisplayName = "Ken Adams"
     CompactName = random.choices(string.ascii_lowercase, k=5)
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -31,5 +33,30 @@ def browser(request):
     driver.set_page_load_timeout(20)
     driver.get(LoginData.url)
     request.cls.driver = driver
-    #yield
-    #driver.quit()
+    yield
+    driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
+        # always add url to report
+        extra.append(pytest_html.extras.url("https://s2.usw2.qa.lightning-bolt.com/"))
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            report_directory = os.path.dirname(item.config.option.htmlpath)
+            file_name = str(int(round(time.time() * 1000))) + ".png"
+            # file_name = report.nodeid.replace("::", "_") + ".png"
+            destinationFile = os.path.join(report_directory, file_name)
+            driver.save_screenshot(destinationFile)
+            extra.append(pytest_html.extras.image(file_name, ''))
+        report.extra = extra
+
+
+def pytest_html_report_title(report):
+    report.title = "LightningBolt Report"
